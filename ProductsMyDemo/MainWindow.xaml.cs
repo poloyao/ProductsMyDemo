c291f441572e16459d1f94dc23b0d89fc4a31dbd
yaoyunting <yysay@live.com>
2016-07-21 17:07:02 +0800

    添加项目文件。

---
+++ ProductsMyDemo/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using DevExpress.Xpf.Ribbon;
+using DevExpress.Xpf.WindowsUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductsMyDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : DXRibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigationPaneView_NavPaneExpandedChanged(object sender, DevExpress.Xpf.NavBar.NavPaneExpandedChangedEventArgs e)
+        {
+            layoutPanel.ItemWidth = GridLength.Auto;
+        }
+
+        private void NavigationPaneView_NavPaneExpandedChanging(object sender, DevExpress.Xpf.NavBar.NavPaneExpandedChangingEventArgs e)
+        {
+            navPanelView.Expander.MaxWidth = e.IsExpanded ? Double.PositiveInfinity : navPanelView.Expander.ActualWidth;
+        }
+
+        private void NavigationFrame_Navigating(object sender, DevExpress.Xpf.WindowsUI.Navigation.NavigatingEventArgs e)
+        {
+            if (e.Cancel) return;
+            NavigationFrame frame = (NavigationFrame)sender;
+            FrameworkElement oldContent = (FrameworkElement)frame.Content;
+            if (oldContent != null)
+            {
+                RibbonMergingHelper.SetMergeWith(oldContent, null);
+                RibbonMergingHelper.SetMergeStatusBarWith(oldContent, null);
+            }
+        }
+
+        private void NavigationFrame_Navigated(object sender, DevExpress.Xpf.WindowsUI.Navigation.NavigationEventArgs e)
+        {
+            FrameworkElement newContent = (FrameworkElement)e.Content;
+            if (newContent != null)
+            {
+               // RibbonMergingHelper.SetMergeWith(newContent, ribbon);
+               // RibbonMergingHelper.SetMergeStatusBarWith(newContent, statusBar);
+            }
+        }
+    }
+}
---
+++ ProductsMyDemo/MainWindowViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Mvvm;
+using DevExpress.Mvvm.Native;
+using DevExpress.Mvvm.POCO;
+using DevExpress.Mvvm.UI;
+using DevExpress.Utils;
+using ProductsMyDemo.Loading;
+
+namespace ProductsMyDemo
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            List<ModuleInfo> modules = new List<ModuleInfo>() {
+                ViewModelSource.Create(() => new ModuleInfo("GridTasksModule", this, "合同管理")).SetIcon("GridTasks"),
+                ViewModelSource.Create(() => new ModuleInfo("GridContactsModule", this, "项目放款")).SetIcon("GridContacts"),
+                ViewModelSource.Create(() => new ModuleInfo("SpreadsheetModule", this, "追/代偿")).SetIcon("Spreadsheet"),
+                ViewModelSource.Create(() => new ModuleInfo("RichEditModule", this, "保费管理")).SetIcon("WordProcessing"),
+                ViewModelSource.Create(() => new ModuleInfo("ReportsModule", this, "设置")).SetIcon("BandedReports")
+            };
+
+            ModuleGroups = new ModuleGroup[] { new ModuleGroup("菜单", modules) };
+        }
+        /// <summary>
+        /// 模块列表
+        /// </summary>
+        public virtual IEnumerable<ModuleGroup> ModuleGroups { get; protected set; }
+        /// <summary>
+        /// 当前选中的模块
+        /// </summary>
+        public virtual ModuleInfo SelectedModuleInfo { get; set; }
+
+        /// <summary>
+        /// 加载过场界面
+        /// </summary>
+        public virtual Type SplashScreenType { get; set; }
+
+        protected virtual INavigationService NavigationService { get { return null; } }
+
+        protected virtual IApplicationJumpListService ApplicationJumpListService { get { return null; } }
+
+        /// <summary>
+        /// 模块初始化
+        /// </summary>
+        public void OnModulesLoaded()
+        {
+            if (SelectedModuleInfo == null)
+            {
+                SelectedModuleInfo = ModuleGroups.First().ModuleInfos.First();
+                SelectedModuleInfo.IsSelected = true;
+                SelectedModuleInfo.Show();
+            }
+
+            SplashScreenType = typeof(ProgressWindow);
+            //ApplicationJumpListService.Items.AddOrReplace("")
+
+        }
+
+
+        public class ModuleGroup
+        {
+            public ModuleGroup(string _title, IEnumerable<ModuleInfo> _moduleInfos)
+            {
+                Title = _title;
+                ModuleInfos = _moduleInfos;
+            }
+            public string Title { get; private set; }
+            public IEnumerable<ModuleInfo> ModuleInfos { get; private set; }
+        }
+        public class ModuleInfo
+        {
+            ISupportServices parent;
+
+            public ModuleInfo(string _type, object parent, string _title)
+            {
+                Type = _type;
+                this.parent = (ISupportServices)parent;
+                Title = _title;
+            }
+            public string Type { get; private set; }
+            public virtual bool IsSelected { get; set; }
+            public string Title { get; private set; }
+            public virtual Uri Icon { get; set; }
+            public ModuleInfo SetIcon(string icon)
+            {
+                this.Icon = AssemblyHelper.GetResourceUri(typeof(ModuleInfo).Assembly, string.Format("Images/{0}.png", icon));
+                return this;
+            }
+
+            /// <summary>
+            /// 显示当前模块
+            /// </summary>
+            /// <param name="parameter"></param>
+            public void Show(object parameter = null)
+            {
+                INavigationService navigationService = parent.ServiceContainer.GetRequiredService<INavigationService>();
+                navigationService.Navigate(Type, parameter, parent);
+            }
+        }
+    }
+}
